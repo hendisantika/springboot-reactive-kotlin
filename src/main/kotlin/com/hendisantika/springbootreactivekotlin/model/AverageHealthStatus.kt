ea2a7fcfd6fe4fee54c974a4241c7062ad59d22d
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-01 09:15:17 +0700

    #14 - Add AverageHealthStatus Model

---
+++ src/main/kotlin/com/hendisantika/springbootreactivekotlin/model/AverageHealthStatus.kt
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootreactivekotlin.model
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-reactive-kotlin
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 01/05/20
+ * Time: 09.15
+ */
+class AverageHealthStatus(var cnt: Int, var temperature: Double, var bloodPressure: Double, var heartRate: Double)
