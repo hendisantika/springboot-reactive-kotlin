f756818fcaaaef90c73cf66f7a9feb4a899bb0f1
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-01 10:29:13 +0700

    #19 - Add whenRequestProfile_thenStatusShouldBeOk Test Case

--- src/test/kotlin/com/hendisantika/springbootreactivekotlin/SpringbootReactiveKotlinApplicationTests.kt
+++ src/test/kotlin/com/hendisantika/springbootreactivekotlin/SpringbootReactiveKotlinApplicationTests.kt
@@ -1,13 +1,38 @@
 package com.hendisantika.springbootreactivekotlin
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.hendisantika.springbootreactivekotlin.controller.ProfileController
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.reactive.server.WebTestClient
 
 @SpringBootTest
 class SpringbootReactiveKotlinApplicationTests {
+    @Autowired
+    lateinit var controller: ProfileController
+
+    @Autowired
+    lateinit var mapper: ObjectMapper
+
+    lateinit var client: WebTestClient
+    lateinit var profile: String
+
+    @BeforeEach
+    fun setup() {
+        client = WebTestClient.bindToController(controller).build()
+    }
 
     @Test
-    fun contextLoads() {
+    fun whenRequestProfile_thenStatusShouldBeOk() {
+        client.post()
+                .uri("/profile")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(profile)
+                .exchange()
+                .expectStatus().isOk
     }
 
 }
