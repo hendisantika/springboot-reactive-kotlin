717c73e186461503690d5fb57ba6dea6814a6f87
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-01 07:35:40 +0700

    #1 - Initial Commit

---
+++ src/main/kotlin/com/hendisantika/springbootreactivekotlin/SpringbootReactiveKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.hendisantika.springbootreactivekotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootReactiveKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootReactiveKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/hendisantika/springbootreactivekotlin/SpringbootReactiveKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootreactivekotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootReactiveKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
